d30170e4d4b74a586de3e44dc6aba6244b1baf9b
Swarzstein <zorrillagja@gmail.com>
2024-06-25 21:47:36 -0400

    bug when searching for same product fixed

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,28 +1,34 @@
 class SearchesController < ApplicationController
   before_action :require_user
   before_action :set_current_user
+  before_action :initial_search, only: [:index]
 
   def index
     @searches = Search.where(user_id: @current_user.id).order(created_at: :desc).limit(10)
-    # @global_searches = Search.all
   end
 
   def create
     @last_search = last_search
     @search = Search.new(search_params)
-    if @last_search.nil? || @search.query.include?(@last_search.query) == false
+    if @last_search.nil? || @search.query.include?(@last_search.query) == false || @search.query == @last_search.query
       if @search.query != ""
         @search.user_id = @current_user.id	
         @search.save
-        @last_search = @search
+      else
+        @last_search = nil
       end
     else
       @last_search.query = @search.query
       @last_search.save
     end
+    render json: { search: @search.query }
   end
 
   private
+
+  def initial_search
+    @last_search = nil
+  end
 
   def set_current_user
     @current_user = current_user
--- app/javascript/liveSearch.js
+++ app/javascript/liveSearch.js
@@ -6,7 +6,6 @@ document.addEventListener('turbo:load', () => {
 
     clearTimeout(e.target.delay);
     e.target.delay = setTimeout(function() {
-      // console.log(query);
       fetch(`/searches`, {
         method: 'POST',
         headers: {
@@ -14,14 +13,18 @@ document.addEventListener('turbo:load', () => {
           'X-CSRF-Token': document.querySelector('[name="csrf-token"]').content
         },
         body: JSON.stringify({ query })
+      })
+      .then(response => response.json())
+      .then((data) => {
+        console.log(data);
+        let newSearch = `<li>${data.search}</li>`;
+        searchList.insertAdjacentHTML('afterbegin', newSearch);
+        if (searchList.children.length > 10 && query !== "") {
+          searchList.lastElementChild.remove();
+        }
       });
-      let newSearch = `<li>${query}</li>`;
-      searchList.insertAdjacentHTML('afterbegin', newSearch);
-      if (searchList.lastElementChild && query !== "") {
-        searchList.lastElementChild.remove();
-      }
-    }, 700);
-
+    }, 650);
   });
 });
-// The code above is a simple JavaScript snippet that listens for the keyup event on the search input field. When a user types a query, the code sends a POST request to the /searches endpoint with the query as the request body. The server will then process the request and return the search results as JSON data.
+
+// The code above is a simple JavaScript snippet that listens for the keyup event on the search input field. When a user types a query, the code sends a POST request to the /searches endpoint with the query as the request body. The server will then process the request and return the search results as JSON data retrieving it to the DOM for mock live search.  
