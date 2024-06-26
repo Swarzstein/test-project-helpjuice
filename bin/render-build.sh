3495f268810c5d61235563d97f59fc639c3e0445
Swarzstein <zorrillagja@gmail.com>
2024-06-26 11:45:43 -0400

    db:seed

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -7,4 +7,4 @@ bundle lock --add-platform x86_64-linux
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec rails db:migrate db:setup
+bundle exec rails db:seed
